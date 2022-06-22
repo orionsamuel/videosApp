b9e7258dc7446ae1fc3f332071404519f78f69e4
Samuel Oliveira <s.oliveira.15@hotmail.com>
2022-06-22 01:17:20 -0300

    Declarando variáveis e criando interfaces com Angular

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,27 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos App';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '04/15/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/nkayOAUBUu4mMvyNf9iHSUiPjF1.jpg',
+      generos: ['Action', 'Fantasy', 'Adventure']
+    },
+    {
+      nome: 'Justice League (2021)',
+      lancamento: '03/18/2021',
+      duracao: '4h 02m',
+      classificacao: 83,
+      cartaz: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg',
+      generos: ['Action', 'Adventure', 'Fantasy', 'Science Fiction']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
